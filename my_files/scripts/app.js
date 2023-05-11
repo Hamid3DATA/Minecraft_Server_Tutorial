074cb6aad4e4e13fefabbdd557231fcb55c29f90
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2023-05-11 10:20:59 +0200

    Add files via upload

---
+++ my_files/scripts/app.js
@@ -0,0 +1,83 @@
+function copy1() {
+    var copyText = document.getElementById("content_command_text1");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy2() {
+    var copyText = document.getElementById("content_command_text2");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy3() {
+    var copyText = document.getElementById("content_command_text3");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy4() {
+    var copyText = document.getElementById("content_command_text4");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy5() {
+    var copyText = document.getElementById("content_command_text5");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy6() {
+    var copyText = document.getElementById("content_command_text6");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy7() {
+    var copyText = document.getElementById("content_command_text7");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy8() {
+    var copyText = document.getElementById("content_command_text8");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy9() {
+    var copyText = document.getElementById("content_command_text9");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy10() {
+    var copyText = document.getElementById("content_command_text10");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy11() {
+    var copyText = document.getElementById("content_command_text11");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy12() {
+    var copyText = document.getElementById("content_command_text12");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy13() {
+    var copyText = document.getElementById("content_command_text13");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
+
+function copy14() {
+    var copyText = document.getElementById("content_command_text14");
+
+    navigator.clipboard.writeText(copyText.innerText);
+}
